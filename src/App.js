3b590e45142b40a1780a3d527d98feba4c260970
MateusCarvalhais <mateus.sardinha.carvalhais@gmail.com>
2024-02-27 15:38:05 -0300

    aula1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  //contador
+  const [ useEffectCounter, SetUseEffectCounter] = useState(0);
+  // estou logado ou nao ?
+  const [logado, setLogado] = useState(false)
+
+  useEffect(()=>{
+    console.log("useeffect foi chamado");
+    SetUseEffectCounter(useEffectCounter + 1) // mudando o valor da tela para outro
+
+  },[logado]);
+
+  const Logar = () =>{
+    setLogado(true); // mudando o estado da variavel por debaixo dos panos
+  }
+
+  const Deslogar = () =>{
+    setLogado(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>O UseEffect foi chamado {useEffectCounter} vezes</h1>
+       {logado ? <p>Estou logado</p> : <p>Estou Deslogado</p>}
+       <button onClick={Logar}>Logar</button>
+       <button onClick={Deslogar}>Deslogar</button>
     </div>
-  );
+  ); 
 }
 
 export default App;
